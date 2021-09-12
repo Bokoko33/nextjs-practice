e1d01a1c6447942ea46f92cdfca8d66751fa9b81
藤田 侑己 <fujita_yuki@MBP20180521-01.local>
2021-09-12 22:16:30 +0900

    ギャラリーページに横スクロールを実装

--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -1,23 +1,42 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
 
 export const Footer = () => {
+  const router = useRouter();
+
   return (
-    <footer css={styles.footer}>
+    <footer
+      css={
+        router.pathname === '/gallery'
+          ? styles.fixedFooter
+          : styles.defaultFooter
+      }
+    >
       <p css={styles.copyright}>&copy; 2021 boko</p>
     </footer>
   );
 };
 
+const footerBaseStyle = css`
+  width: 100%;
+  height: var(--height-footer-pc);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: solid 1px var(--color-black);
+  border-bottom: solid 1px var(--color-black);
+`;
+
 const styles = {
-  footer: css`
-    width: 100%;
-    height: var(--height-footer-pc);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-top: solid 1px var(--color-black);
-    border-bottom: solid 1px var(--color-black);
+  defaultFooter: css`
+    ${footerBaseStyle}
+  `,
+  fixedFooter: css`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    ${footerBaseStyle}
   `,
   copyright: css`
     font-size: 12px;
--- pages/gallery/index.jsx
+++ pages/gallery/index.jsx
@@ -1,7 +1,134 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import Image from 'next/image';
+import { useState, useRef, useEffect } from 'react';
+
+// 開始と終了をなめらかに補間する関数
+const lerp = (start, end, multiplier) => {
+  return (1 - multiplier) * start + multiplier * end;
+};
+
+// 両端を過不足ないようにするためのスクロール領域計算
+const calcBodyHeight = (containerWidth, windowWidth, windowHeight) => {
+  return windowHeight + containerWidth - windowWidth;
+};
+
 export default function Gallery() {
+  const images = [
+    { title: 'street', src: '/images/image01.jpg' },
+    { title: 'shade', src: '/images/image02.jpg' },
+    { title: 'station', src: '/images/image03.jpg' },
+    { title: 'car', src: '/images/image04.jpg' },
+    { title: 'street', src: '/images/image01.jpg' },
+    { title: 'shade', src: '/images/image02.jpg' },
+    { title: 'station', src: '/images/image03.jpg' },
+    { title: 'car', src: '/images/image04.jpg' },
+  ];
+
+  const scrollAreaEl = useRef(null);
+  const requestAnimationRef = useRef(null);
+
+  useEffect(() => {
+    const htmlEl = document.documentElement;
+    const bodyEl = document.body;
+
+    // ページアクセス時にぐい〜んってならないようにuseEffect内で初期化したい
+    let currentScrollY = 0;
+    // let scrollValue = 0;
+
+    const setBodyHeight = () => {
+      const virtualHeight = calcBodyHeight(
+        scrollAreaEl.current.clientWidth,
+        window.innerWidth,
+        window.innerHeight
+      );
+      bodyEl.style.height = `${virtualHeight}px`;
+    };
+
+    const loop = () => {
+      // スクロール位置を更新
+      currentScrollY = lerp(currentScrollY, htmlEl.scrollTop, 0.1);
+      // scrollValue = htmlEl.scrollTop - currentScrollY;
+
+      if (scrollAreaEl.current) {
+        scrollAreaEl.current.style.transform = `translate3d(${-currentScrollY}px,0,0)`;
+      }
+      requestAnimationRef.current = window.requestAnimationFrame(loop);
+    };
+
+    window.addEventListener('resize', setBodyHeight);
+    setBodyHeight();
+    loop();
+
+    return () => {
+      // useEffectのreturn内の処理が、コンポーネントのunMount直前に呼ばれる
+      window.removeEventListener('resize', setBodyHeight);
+      window.cancelAnimationFrame(requestAnimationRef.current);
+
+      // bodyの高さをリセット
+      bodyEl.style.height = 'auto';
+    };
+  }, []);
+
   return (
-    <>
-      <h1>Galleryページ</h1>
-    </>
+    <div css={styles.fixed}>
+      <div css={styles.scrollable} ref={scrollAreaEl}>
+        <ul css={styles.list}>
+          {images.map(({ title, src }, index) => (
+            <li css={styles.item} key={index}>
+              <Image
+                src={src}
+                layout={'fill'}
+                objectFit={'cover'}
+                alt={title}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
+
+const styles = {
+  container: css`
+    position: relative;
+    width: 100%;
+  `,
+  fixed: css`
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+  `,
+  scrollable: css`
+    position: absolute;
+    height: 100%;
+    padding: var(--height-header-pc) 120px var(--height-footer-pc);
+    top: 0;
+    left: 0;
+  `,
+  list: css`
+    height: 100%;
+    display: flex;
+    align-items: center;
+  `,
+  item: css`
+    position: relative;
+    width: 500px;
+    height: 320px;
+    &:not(:first-of-type) {
+      margin-left: 120px;
+    }
+
+    &:nth-of-type(2n) {
+      margin-top: -20vh;
+    }
+
+    &:nth-of-type(2n + 1) {
+      margin-top: 20vh;
+    }
+  `,
+};
